c8b59aaf9caaee436b4158a301fbb30aaa6f8700
piyushpc <190106029@hbtu.ac.in>
2024-09-03 22:18:06 +0530

    Updated deploy.yml and other necessary files

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
