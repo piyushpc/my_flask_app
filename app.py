014eec3f544bf15f285c5c670d1b846ac76dac3f
piyushpc <190106029@hbtu.ac.in>
2024-09-05 01:31:55 +0530

    Save progress before break

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import sqlite3
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
 DATABASE = 'mydatabase.db'
 
 def get_db():
